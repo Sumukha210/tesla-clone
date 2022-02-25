b3b43e150dd1dcde646d2517331cdd29da83834b
Sumukha210 <sumukhakb@gmail.com>
2022-02-25 16:38:49 +0530

    semi info is done

--- src/components/layouts/semi_page/Specification.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SpecificationSection = () => {
-  return <div>SpecificationSection</div>;
-};
-
-export default SpecificationSection;
--- src/pages/semi.tsx
+++ src/pages/semi.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "@/shared/Layout";
 import Hero from "@/layout/semi_page/hero";
+import SemiInfo from "@/layout/semi_page/SemiInfo";
 
 const SemiPage = () => {
   return (
     <Layout>
       <Hero />
+      <SemiInfo />
     </Layout>
   );
 };
